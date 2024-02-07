2a60fdbd142dd00a974b7c63e054465489772e90
Hamrez95 <hamidrezapakpour95@gmail.com>
2024-02-07 18:46:50 +0330

    User , IUserRepository, User.hbm , UnitOfWork for User  All Sets

---
+++ Source/Core/Domain/Concrete/Schema/Accounting/User.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Concrete.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Concrete.Schema.Accounting
+{
+    public class User : BaseEntity
+    {
+        /// <summary>
+        /// موجودیت کاربر
+        /// </summary>
+        public User() 
+        { 
+           Fullname = string.Empty;
+            Username = string.Empty;
+            Password = string.Empty;
+            Mobile = string.Empty;
+            Email = string.Empty;
+            ProfilePhoto = string.Empty;
+            CreationDate = DateTime.Now;
+            IsDeleted = false;
+            Role = null!;
+        }
+        public virtual int Id { get; set; }
+        public virtual string Fullname { get; set; }
+        public virtual string Username { get; set; }
+        public virtual string Password { get; set; }
+        public virtual string Mobile { get; set; }
+        public virtual string Email { get; set; }
+        public virtual byte RoleId { get; set; }
+        public virtual string ProfilePhoto { get; set; }
+        public virtual DateTime CreationDate { get; set; }
+        public virtual bool IsDeleted { get; set; }
+        public virtual Role Role { get; set; }
+    }
+}
---
+++ Source/Core/Domain/Contract/Schema/Accounting/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Concrete.Schema.Accounting;
+using Domain.Contract.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Contract.Schema.Accounting
+{
+    public interface IUserRepository : IBaseRepository<User>
+    {
+    }
+}
