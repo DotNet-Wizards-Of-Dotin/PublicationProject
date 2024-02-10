275fec8aa2aff1e56968ebd0d7f3c43bef1665e8
Mohammad Amin Ghaderi <m.a.ghaderi78@gmail.com>
2024-02-10 23:05:42 +0330

    structure and architecture of project

---
+++ src/4.BookStoreManagement.EndPoint/BM.EndPoint/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BM.EndPoint.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
